7efaa6d45c9b9dd63076fbd8810554f81c9eba82
unknown <a.alex.segal@gmail.com>
2024-12-10 18:57:26 -0500

    Got it to work as long as there's no double wars in which I somehow gain 2 cards or end with war in which I lose 2 cards

--- peace.py
+++ peace.py
@@ -47,13 +47,11 @@ def play_round(player1_hand, player2_hand,war_list):
         player2_hand.append(p2_card)
         player2_hand.append(p1_card)
     else:
-        war_list.insert(0,p1_card)
-        war_list.insert(0,p2_card)
-        war(player1_hand,player2_hand,war_list)
+        war(player1_hand,player2_hand,war_list,p1_card,p2_card)
     print(len(player1_hand))
     print(len(player2_hand))
     return player1_hand, player2_hand
-def war(player1_hand, player2_hand,war_list):
+def war(player1_hand, player2_hand,war_list,p1_card,p2_card):
     """Handle the 'war' scenario when cards are equal.
 		recall the rules of war, both players put 3 cards face down, 
 		then both players flip face up a 4th card. The player with the stronger
@@ -62,16 +60,20 @@ def war(player1_hand, player2_hand,war_list):
     # Your code here
     war_list = []
     result = 0
+    war_list.insert(0,p2_card)
+    war_list.insert(0,p1_card)
     while result != 1 and result != 2:
         if len(player1_hand) <5:
+            #player2_hand.extend(player1_hand)
             player1_hand = []
-            return player1_hand
+            return player1_hand, player2_hand
         elif len(player2_hand) <5:
+            #player1_hand.extend(player2_hand)
             player2_hand = []
-            return player2_hand
+            return player2_hand, player1_hand
         else:
-            war_list += [player1_hand.pop(0) for _ in range(4)]
-            war_list += [player2_hand.pop(0) for _ in range(4)]
+            war_list += [player1_hand.pop(0) for _ in range(3)]
+            war_list += [player2_hand.pop(0) for _ in range(3)]
             p1_card = player1_hand.pop(0)
             print(p1_card)
             p2_card = player2_hand.pop(0)
@@ -82,9 +84,12 @@ def war(player1_hand, player2_hand,war_list):
                 player1_hand.append(p2_card)
                 player1_hand.extend(war_list)
             elif result == 2:
-                player2_hand.append(p2_card)
                 player2_hand.append(p1_card)
+                player2_hand.append(p2_card)
                 player2_hand.extend(war_list)
+            else:
+                war_list.append(p1_card)
+                war_list.append(p2_card)
     return war_list, player1_hand, player2_hand
 def play_game():
     """Main function to run the game."""
